20debe9fa2baaa1d60c4cf6f5a5538292e898597
Vanmathi Y <vandataengineer@gmail.com>
2025-08-11 07:36:54 +0530

    Update InterviewQue.py

--- InterviewQue.py
+++ InterviewQue.py
@@ -1,4 +1,4 @@
-#Find the least positive missing num in the list 
+#1. Find the least positive missing num in the list 
 
 A=[1,0,5,2,4,-1,7]
 
@@ -10,3 +10,9 @@ def smallestmissing(A):
     return smallest
 
 print(smallestmissing(A))
+
+
+#2. To remove any special char or punctuations
+re.findall(r"\w+", line) = Split into clean words without punctuation.
+
+#3. 
